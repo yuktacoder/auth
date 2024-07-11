5491c80b441dd482bae6393537beb5bf93be62b4
yuktacoder <manek.yukta@gmail.com>
2024-07-11 04:09:39 +0000

    Initial commit

---
+++ authentication/app/page.tsx
@@ -0,0 +1,16 @@
+
+export default function Home() {
+  return (
+    <main className="flex h-full flex-col items-center justify-center 
+    bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+         <div className="space-y-6 text-center">
+          <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+           Auth
+          </h1>
+          <p className="text-white text-lg">
+             A simple Authentication service 
+          </p>
+         </div>
+         </main>
+  );
+}
